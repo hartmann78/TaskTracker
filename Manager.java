1dd2809d66f2938d584dda17f29b1fb79d7133fd
hartmann78 <164620101+hartmann78@users.noreply.github.com>
2024-09-28 00:04:45 +0600

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,11 +1,12 @@
 package Tracker;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Manager {
-    int id = 0;
-    int subTaskId = 0;
+    int id = 1;
+    int subTaskId = 1;
     Scanner scanner = new Scanner(System.in);
     HashMap<Integer, Task> taskList = new HashMap<>();
 
@@ -18,6 +19,7 @@ public class Manager {
                     }
                 }
                 checkEpic.setStatus(Status.DONE);
+                System.out.println("Эпик " + updateEpic.name + " переведён в статус DONE.");
                 return;
             }
         }
@@ -34,6 +36,7 @@ public class Manager {
         System.out.println("7 - завершить работу");
     }
 
+    // Function 1
     public void showTasks() {
         if (isEmptyCheck()) return;
         for (Task showTask : taskList.values()) {
@@ -43,21 +46,41 @@ public class Manager {
         }
     }
 
+    // Function 2
     public void removeAll() {
         if (isEmptyCheck()) return;
         taskList.clear();
         System.out.println("Все задачи удалены!");
     }
 
+    // Function 3
     public void getById() {
         if (isEmptyCheck()) return;
         while (true) {
             System.out.println("Введите индентификатор:");
-            int getById = scanner.nextInt();
-            if (taskList.containsKey(getById)) {
-                System.out.println("Название: " + taskList.get(getById).name);
-                System.out.println("Описание: " + taskList.get(getById).description);
-                System.out.println("Статус: " + taskList.get(getById).status);
+            int id = scanner.nextInt();
+            if (taskList.containsKey(id)) {
+                System.out.println("Название: " + taskList.get(id).name);
+                System.out.println("Описание: " + taskList.get(id).description);
+                System.out.println("Статус: " + taskList.get(id).status);
+
+                if (taskList.get(id) instanceof Epic epic) {
+                    System.out.println("Желаете ли вы получить список подзадач эпика?");
+                    System.out.println("1 - да. 2 - нет");
+                    String subtaskList = scanner.next();
+
+                    if (subtaskList.equals("1")) {
+                        for (Map.Entry<Integer, Subtask> subtask : epic.subtasks.entrySet()) {
+                            System.out.println("Подзадача " + subtask.getKey());
+                            System.out.println("Название: " + subtask.getValue().name);
+                            System.out.println("Описание: " + subtask.getValue().description);
+                            System.out.println("Статус: " + subtask.getValue().status);
+                        }
+                    }
+                }
+                return;
+            } else if (id == 0) {
+                System.out.println("Выход");
                 return;
             } else {
                 System.out.println("Неверный идентификатор!");
@@ -65,6 +88,7 @@ public class Manager {
         }
     }
 
+    // Function 4
     public void createTask() {
         while (true) {
             System.out.println("Что вы хотите создать?");
@@ -77,7 +101,7 @@ public class Manager {
                     String taskName = scanner.nextLine();
                     System.out.println("Введите описание задачи:");
                     String taskDescription = scanner.nextLine();
-                    taskList.put(++id, new Task(taskName, taskDescription, Status.NEW));
+                    taskList.put(id++, new Task(taskName, taskDescription, Status.NEW));
                     return;
                 case ("2"):
                     System.out.println("Введите название эпика:");
@@ -89,13 +113,18 @@ public class Manager {
                     System.out.println("Сколько подзадач вы желаете добавить?");
                     int subtasks = scanner.nextInt();
                     for (int i = 0; i < subtasks; i++) {
+                        System.out.println("Подзадача " + (i + 1));
                         System.out.println("Введите название подзадачи:");
                         String subtaskName = scanner.nextLine();
                         System.out.println("Введите описание подзадачи:");
                         String subtaskDescription = scanner.nextLine();
-                        newEpic.subtasks.put(++subTaskId, new Subtask(subtaskName, subtaskDescription, Status.NEW));
+                        newEpic.subtasks.put(subTaskId++, new Subtask(subtaskName, subtaskDescription, Status.NEW));
                     }
-                    taskList.put(++id, newEpic);
+                    subTaskId = 1;
+                    taskList.put(id++, newEpic);
+                    return;
+                case ("0"):
+                    System.out.println("Выход");
                     return;
                 default:
                     System.out.println("Неверная команда");
@@ -103,6 +132,7 @@ public class Manager {
         }
     }
 
+    // Function 5
     public void updateStatus() {
         if (isEmptyCheck()) return;
         while (true) {
@@ -110,15 +140,19 @@ public class Manager {
             int selectId = scanner.nextInt();
             if (taskList.containsKey(selectId)) {
                 if (taskList.get(selectId) instanceof Epic selectSubtask) {
-                    System.out.println("Выберите подзадачу:");
+                    System.out.println("Список подзадач:");
                     for (Integer key : selectSubtask.subtasks.keySet()) {
                         System.out.println("id: " + key + " - " + selectSubtask.subtasks.get(key).name);
                     }
+                    System.out.println("Выберите подзадачу:");
                     int subtaskId = scanner.nextInt();
-                    setNewStatus( selectSubtask.subtasks.get(subtaskId));
+                    setNewStatus(selectSubtask.subtasks.get(subtaskId));
                 } else {
                     setNewStatus(taskList.get(selectId));
                 }
+                return;
+            } else if (selectId == 0) {
+                System.out.println("Выход");
                 return;
             } else {
                 System.out.println("Неверный идентификатор!");
@@ -133,16 +167,20 @@ public class Manager {
             System.out.println("1 - Новый");
             System.out.println("2 - В процессе");
             System.out.println("3 - Выполнен");
-            String newStatus = scanner.nextLine();
+            String newStatus = scanner.nextLine(); // Во время работы компилятор почему-то игноирует её
+            newStatus = scanner.nextLine();
             switch (newStatus) {
                 case ("1"):
-                    taskList.get(updateId).setStatus(Status.NEW);
+                    updateId.setStatus(Status.NEW);
                     return;
                 case ("2"):
-                    taskList.get(updateId).setStatus(Status.IN_PROGRESS);
+                    updateId.setStatus(Status.IN_PROGRESS);
                     return;
                 case ("3"):
-                    taskList.get(updateId).setStatus(Status.DONE);
+                    updateId.setStatus(Status.DONE);
+                    return;
+                case ("0"):
+                    System.out.println("Выход");
                     return;
                 default:
                     System.out.println("Неверный номер!");
@@ -151,14 +189,18 @@ public class Manager {
         }
     }
 
+    // Function 6
     public void deleteTask() {
         if (isEmptyCheck()) return;
         while (true) {
-            System.out.println("Введите идентификатор который желаете удалить:");
+            System.out.println("Введите идентификатор который желаете удалить: ");
             int deleteId = scanner.nextInt();
             if (taskList.containsKey(deleteId)) {
                 taskList.remove(deleteId);
                 System.out.println("Удаление успешно!");
+                return;
+            } else if (deleteId == 0) {
+                System.out.println("Выход");
                 return;
             } else {
                 System.out.println("Неверный идентификатор!");
